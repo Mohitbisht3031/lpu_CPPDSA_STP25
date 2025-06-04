a511a2950ba7fd552e7039603dcab286aebd81a7
Mohit Bisht <imohiitsingh@gmail.com>
2025-06-04 17:50:04 +0530

    Day0

---
+++ Day0/first.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> //header file
+using namespace std; // namespace group
+
+int main(){  // main function from which all the cpp code execution starts
+    cout<<"Hey welcome to the 0 class using a custom name for the output file"<<endl; // to give output
+
+    // to take an input
+    int a;
+    cin >> a;
+
+    return 0;
+}
